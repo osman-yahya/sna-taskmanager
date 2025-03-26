1ece4278563890de081811ca07bea87c77fdc002
osman-yahya <osmanyahyaakinci@hotmail.com>
2025-03-26 18:16:45 +0300

    acc token 60day e alındı

--- All/TimeSheetPlus/settings.py
+++ All/TimeSheetPlus/settings.py
@@ -158,7 +158,7 @@ AUTHENTICATION_BACKENDS = [
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  # Access token süresi
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=60),  # Access token süresi
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),  # Refresh token süresi
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
